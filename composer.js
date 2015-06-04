7d18c81e494b2b21b991bac70643a2caeb3ceb3e
kurispesso <kurispesso@yandex.ru>
2015-06-04 17:00:45 +0300

    first commit

---
+++ composer.js
@@ -0,0 +1,12 @@
+{
+    "name": "WebDriverJs",
+    "type": "library",
+    "keywords": ["php", "phantomjs", "casperjss"],
+    "homepage": "https://github.com/kurispesso/WebDriverJs",
+    "require": {
+        "php": ">=5.3.0"
+    },
+    "autoload": {
+        "psr-0": {"WebDriverJs": ""}
+    }
+}
